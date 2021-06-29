0d35b58345aaa4c3d747b740ff49579f6c685534
bitsofinfo <bitsofinfo.g@gmail.com>
2021-06-29 15:43:55 +0000

    added vnet

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,7 @@
 module "network-bof" {
   source  = "iacdev.wpp.cloud/WPP-Lab/network-bof/azurerm"
   version = "3.0.1"
+  
   # insert required variables here
   resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
